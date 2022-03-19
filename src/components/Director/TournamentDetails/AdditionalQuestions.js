9c1463bb2736c38a29c25e6b2143abd17cce60a7
Scott Stebleton <scott@stebleton.net>
2022-03-19 16:14:27 -0400

    add edit link for existing additional questions

--- src/components/Director/TournamentDetails/AdditionalQuestions.js
+++ src/components/Director/TournamentDetails/AdditionalQuestions.js
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import Card from 'react-bootstrap/Card';
 import ListGroup from "react-bootstrap/ListGroup";
 
@@ -11,15 +12,30 @@ const additionalQuestions = () => {
     return '';
   }
 
+  const [editing, setEditing] = useState(false);
+
+  const tournamentHasQuestions = context.tournament.additional_questions.length > 0;
+  const editable = context.tournament.state !== 'active' && context.tournament.state !== 'closed';
+  const editLink = (
+    <a href={'#'}
+       className={'ms-auto align-middle'}
+       onClick={() => {}}>
+      <i className={'bi-pencil-fill'} aria-hidden={true}/>
+      <span className={'visually-hidden'}>edit</span>
+    </a>
+  );
+
   return (
     <Card className={classes.Card}>
-      <Card.Header as={'h5'} className={'fw-light'}>
-        Additional Form Questions
+      <Card.Header className={'d-flex'}>
+        <h5 className={'fw-light mb-0'}>
+          Additional Form Questions
+        </h5>
+        {tournamentHasQuestions && editable && editLink}
       </Card.Header>
       <ListGroup variant={'flush'}>
-        {context.tournament.additional_questions.length === 0
-          ? <ListGroup.Item>None configured</ListGroup.Item>
-          : context.tournament.additional_questions.map((q, i) => {
+        {!tournamentHasQuestions && <ListGroup.Item>None configured</ListGroup.Item>}
+        {tournamentHasQuestions && context.tournament.additional_questions.map((q, i) => {
           return (
             <ListGroup.Item key={i}>
               {q.label}
