2104123ae81520c0914c8919c1dd4836c5fd3147
Scott Stebleton <scott@stebleton.net>
2022-03-19 22:17:27 -0400

    finish up manipulation of additional question, sans validation of order

--- src/components/Director/TournamentDetails/AdditionalQuestions.js
+++ src/components/Director/TournamentDetails/AdditionalQuestions.js
@@ -1,75 +1,145 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import {useRouter} from "next/router";
 import Card from 'react-bootstrap/Card';
 import ListGroup from "react-bootstrap/ListGroup";
-// import {DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import {useDirectorContext} from "../../../store/DirectorContext";
 import AdditionalQuestionForm from "../AdditionalQuestionForm/AdditionalQuestionForm";
 
 import classes from './AdditionalQuestions.module.scss';
+import {directorApiRequest} from "../../../utils";
 
 const additionalQuestions = () => {
   const context = useDirectorContext();
+  const router = useRouter();
   if (!context || !context.tournament) {
     return '';
   }
 
+  const initialSaveData = {
+    questions: context.tournament.additional_questions.map(q => ({ id: q.id, order: q.order, _destroy: false })),
+    valid: true, // currently, no validity is enforced, so it's always true
+  }
+
   const [editing, setEditing] = useState(false);
+  const [saveData, setSaveData] = useState(initialSaveData);
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const tournamentHasQuestions = context.tournament.additional_questions.length > 0;
   const editable = context.tournament.state !== 'active' && context.tournament.state !== 'closed';
+
+  useEffect(() => {
+    const dataFromContext = {
+      questions: context.tournament.additional_questions.map(q => ({ id: q.id, order: q.order, _destroy: false })),
+      valid: true,
+    };
+    setSaveData(dataFromContext);
+  }, [context]);
 
   const editClicked = (event) => {
     event.preventDefault();
     setEditing(true);
   }
 
-  const saveClicked = (event) => {
-    event.preventDefault();
+  const onSaveSuccess = (data) => {
     setEditing(false);
+    setSuccessMessage(
+      <div className={'alert alert-success alert-dismissible fade show d-flex align-items-center my-2'} role={'alert'}>
+        <i className={'bi-check2-circle pe-2'} aria-hidden={true} />
+        <div className={'me-auto'}>
+          Questions saved.
+          <button type="button"
+                  className={"btn-close"}
+                  data-bs-dismiss="alert"
+                  onClick={() => setSuccessMessage(null)}
+                  aria-label="Close" />
+        </div>
+      </div>
+    );
+    context.setTournament(data);
   }
 
-  const editLink = (
-    <button
-       className={`${classes.EditIcon} btn btn-sm btn-outline-primary ms-auto align-middle`}
-       onClick={editClicked}>
-      <i className={'bi-pencil'} aria-hidden={true}/>
-      <span className={'visually-hidden'}>edit</span>
-    </button>
-  );
-  const saveLink = (
-    <button
-       className={`${classes.SaveIcon} btn btn-sm btn-secondary ms-auto align-middle`}
-       onClick={saveClicked}>
-      <i className={'bi-pencil-fill'} aria-hidden={true}/>
-      <span className={'visually-hidden'}>save</span>
-    </button>
-  )
+  const onSaveFailure = (data) => {
+    console.log(':(', data);
+    setErrorMessage(
+      <div className={'alert alert-danger alert-dismissible fade show d-flex align-items-center my-2'} role={'alert'}>
+        <i className={'bi-check2-circle pe-2'} aria-hidden={true} />
+        <div className={'me-auto'}>
+          Failed to save: {data.error}
+          <button type="button"
+                  className={"btn-close"}
+                  data-bs-dismiss="alert"
+                  onClick={() => setErrorMessage(null)}
+                  aria-label="Close" />
+        </div>
+      </div>
+    );
+  }
 
-  let list = (
-    context.tournament.additional_questions.map((q, i) => (
-      <ListGroup.Item key={i}>
-        {q.label}
-      </ListGroup.Item>
-    ))
-  );
+  const saveClicked = (event) => {
+    event.preventDefault();
+
+    const patchData = {
+      tournament: {
+        additional_questions_attributes: saveData.questions,
+      }
+    }
+    const uri = `/director/tournaments/${context.tournament.identifier}`;
+    const requestConfig = {
+      method: 'patch',
+      data: patchData,
+    }
+    directorApiRequest({
+      uri: uri,
+      requestConfig: requestConfig,
+      context: context,
+      router: router,
+      onSuccess: onSaveSuccess,
+      onFailure: onSaveFailure,
+    })
+  }
+
+  const inputChanged = (event, i) => {
+    const newSaveData = {...saveData};
+    newSaveData.questions[i].order = parseInt(event.target.value);
+    setSaveData(newSaveData);
+  }
+
+  const deleteClicked = (event, i) => {
+    event.preventDefault();
+    const newSaveData = {...saveData};
+    newSaveData.questions[i]._destroy = true;
+    setSaveData(newSaveData);
+  }
+
+  let list = '';
   if (editing) {
     list = (
       context.tournament.additional_questions.map((q, i) => {
-        const str = q.label.length < 30 ? q.label : q.label.substring(0, 30).concat('...');
+        const str = q.label.length < 25 ? q.label : q.label.substring(0, 25).concat('...');
+        if (!saveData.questions[i] || saveData.questions[i]._destroy) {
+          return '';
+        }
         return (
           <ListGroup.Item key={i} className={'d-flex justify-content-start'}>
-            <div className={classes.GrabHandle}>
-              <i className={'bi-chevron-expand pe-3'} aria-hidden={true}/>
-              <span className={'visually-hidden'}>Reorder</span>
-            </div>
-            <div>
+            <input type={'number'}
+                   value={saveData.questions[i].order}
+                   onChange={(event) => inputChanged(event, i)}
+                   min={1}
+                   max={context.tournament.additional_questions.length}
+                   className={`${classes.OrderInput} form-control`}
+                   name={`aq-order-${i}`}
+                   id={`aq-order-${i}`}
+                   title={'blah blah'}
+                   />
+            <div className={'form-control-plaintext ms-3'}>
               {str}
             </div>
             <div title={'delete'} className={'ms-auto'}>
-              <a className={'text-danger'}
+              <a className={'text-danger form-control-plaintext'}
                       href={'#'}
-                      onClick={() => {}}>
+                      onClick={(event) => deleteClicked(event, i)}>
                 <i className={'bi-x-circle-fill'} aria-hidden={true} />
                 <span className={'visually-hidden'}>Delete</span>
               </a>
@@ -78,7 +148,49 @@ const additionalQuestions = () => {
         );
       })
     )
+  } else {
+    list = (
+      context.tournament.additional_questions.map((q, i) => {
+        if (saveData.questions[i] && saveData.questions[i]._destroy) {
+          return '';
+        }
+        return (
+          <ListGroup.Item key={i}>
+            <strong>
+              {q.order}{': '}
+            </strong>
+            {q.label}
+          </ListGroup.Item>
+        );
+      })
+    );
   }
+
+  if (successMessage) {
+    list.push(<ListGroup.Item key={list.length + 1}>{successMessage}</ListGroup.Item>);
+  }
+  if (errorMessage) {
+    list.push(<ListGroup.Item key={list.length + 1}>{errorMessage}</ListGroup.Item>);
+  }
+
+  const editLink = (
+    <button
+      className={`${classes.EditIcon} btn btn-sm btn-outline-primary ms-auto align-middle`}
+      onClick={editClicked}>
+      <i className={'bi-pencil'} aria-hidden={true}/>
+      <span className={'visually-hidden'}>edit</span>
+    </button>
+  );
+
+  const saveLink = (
+    <button
+      className={`${classes.SaveIcon} btn btn-sm btn-secondary ms-auto align-middle`}
+      disabled={!saveData.valid}
+      onClick={saveClicked}>
+      <i className={'bi-pencil-fill'} aria-hidden={true}/>
+      <span className={'visually-hidden'}>save</span>
+    </button>
+  );
 
   return (
     <div className={classes.AdditionalQuestions}>
