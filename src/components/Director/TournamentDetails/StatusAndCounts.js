cf404335b71ed35d123c7d14777781285d649766
Scott Stebleton <scott@stebleton.net>
2022-04-01 17:48:15 -0400

    add button to send payment reminders to bowlers who have yet to pay

--- src/components/Director/TournamentDetails/StatusAndCounts.js
+++ src/components/Director/TournamentDetails/StatusAndCounts.js
@@ -23,6 +23,7 @@ const StatusAndCounts = ({testEnvironmentUpdated}) => {
   const [loading, setLoading] = useState(false);
   const [testEnvFormData, setTestEnvFormData] = useState(testEnvFormInitialData);
   const [testEnvSuccessMessage, setTestEnvSuccessMessage] = useState(null);
+  const [paymentReminderMessage, setPaymentReminderMessage] = useState(null);
 
   // Update the state of testEnvFormData with what's in context
   useEffect(() => {
@@ -95,27 +96,6 @@ const StatusAndCounts = ({testEnvironmentUpdated}) => {
     });
   }
 
-  const downloads = (
-    <Card.Body className={'bg-white text-dark'}>
-      <Card.Subtitle className={'mb-3'}>
-        Downloads
-      </Card.Subtitle>
-      <Card.Link className={'btn btn-sm btn-outline-primary'}
-                 href={'#'}
-                 onClick ={(event) => downloadClicked(event, `/director/tournaments/${context.tournament.identifier}/csv_download`, 'bowlers.csv')}
-      >
-        CSV
-      </Card.Link>
-      <Card.Link className={'btn btn-sm btn-outline-primary'}
-                 href={'#'}
-                 onClick ={(event) => downloadClicked(event, `/director/tournaments/${context.tournament.identifier}/igbots_download`, 'bowlers.xml')}
-      >
-        IGBO-TS
-      </Card.Link>
-      {downloadMessage}
-    </Card.Body>
-  );
-
   const counts = (
     <ListGroup variant={'flush'}>
       <ListGroup.Item className={'d-flex'}
@@ -144,6 +124,99 @@ const StatusAndCounts = ({testEnvironmentUpdated}) => {
       </ListGroup.Item>
     </ListGroup>
   );
+
+  const downloads = (
+    <Card.Body className={'bg-white text-dark'}>
+      <Card.Subtitle className={'mb-3'}>
+        Downloads
+      </Card.Subtitle>
+      <Card.Link className={'btn btn-sm btn-outline-primary'}
+                 href={'#'}
+                 onClick ={(event) => downloadClicked(event, `/director/tournaments/${context.tournament.identifier}/csv_download`, 'bowlers.csv')}
+      >
+        CSV
+      </Card.Link>
+      <Card.Link className={'btn btn-sm btn-outline-primary'}
+                 href={'#'}
+                 onClick ={(event) => downloadClicked(event, `/director/tournaments/${context.tournament.identifier}/igbots_download`, 'bowlers.xml')}
+      >
+        IGBO-TS
+      </Card.Link>
+      {downloadMessage}
+    </Card.Body>
+  );
+
+  const paymentRemindersKickedOff = (data) => {
+    setPaymentReminderMessage(
+      <div className={'alert alert-success alert-dismissible fade show d-flex align-items-center mt-3 mb-0'} role={'alert'}>
+        <i className={'bi-check2-circle pe-2'} aria-hidden={true} />
+        <div className={'me-auto'}>
+          Sending payment reminders to bowlers with outstanding balances.
+          <button type="button"
+                  className={"btn-close"}
+                  data-bs-dismiss="alert"
+                  onClick={() => setPaymentReminderMessage(null)}
+                  aria-label="Close" />
+        </div>
+      </div>
+    );
+  }
+
+  const paymentRemindersFailed = (data) => {
+    setPaymentReminderMessage(
+      <div className={'alert alert-warning alert-dismissible fade show d-flex align-items-center mt-3 mb-0'} role={'alert'}>
+        <i className={'bi-exclamation-triangle pe-2'} aria-hidden={true} />
+        <div className={'me-auto'}>
+          Failed to send payment reminders to bowlers with outstanding balances. Please let Scott know.
+          <button type="button"
+                  className={"btn-close"}
+                  data-bs-dismiss="alert"
+                  onClick={() => setPaymentReminderMessage(null)}
+                  aria-label="Close" />
+        </div>
+      </div>
+    );
+  }
+
+  const paymentReminderClickHandler = (event) => {
+    event.preventDefault();
+    if (!['active', 'closed'].includes(context.tournament.state)) {
+      console.log('Cannot send payment reminders for a tournament in setup or test mode.');
+      return;
+    }
+
+    if (!confirm('This will send potentially many emails. Are you sure?')) {
+      return;
+    }
+
+    const uri = `/director/tournaments/${context.tournament.identifier}/email_payment_reminders`;
+    const requestConfig = {
+      data: {},
+      method: 'post',
+    }
+    directorApiRequest({
+      uri: uri,
+      requestConfig: requestConfig,
+      context: context,
+      router: router,
+      onSuccess: paymentRemindersKickedOff,
+      onFailure: paymentRemindersFailed,
+    });
+  }
+
+  const actions = ['active', 'closed'].includes(context.tournament.state) && (
+    <Card.Body className={'bg-white text-dark'}>
+      <Card.Subtitle className={'mb-3'}>
+        Actions
+      </Card.Subtitle>
+      <Card.Link className={'btn btn-sm btn-outline-dark'}
+                 href={'#'}
+                 onClick={paymentReminderClickHandler}>
+        Send Payment Reminder Email
+      </Card.Link>
+      {paymentReminderMessage}
+    </Card.Body>
+  )
 
   const onClearTestDataSuccess = (_) => {
     setLoading(false);
@@ -337,6 +410,7 @@ const StatusAndCounts = ({testEnvironmentUpdated}) => {
       {counts}
       {frontPageLink}
       {downloads}
+      {actions}
       {testingStatusContent}
       {clearTestData}
     </Card>
