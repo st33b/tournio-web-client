9d055a0679c9a781fdd212d3fd9bcf2a5f88ebbd
Scott Stebleton <scott@stebleton.net>
2022-04-29 12:55:17 -0400

    add error boundary to configuration component

--- src/components/Director/TournamentDetails/Configuration.js
+++ src/components/Director/TournamentDetails/Configuration.js
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import classes from './TournamentDetails.module.scss';
 import {useDirectorContext} from "../../../store/DirectorContext";
 import ConfigItemForm from "../ConfigItemForm/ConfigItemForm";
+import ErrorBoundary from "../../common/ErrorBoundary";
 
 const Configuration = () => {
   const context = useDirectorContext();
@@ -11,20 +12,22 @@ const Configuration = () => {
   }
 
   return (
-    <Card className={`${classes.Card} ${classes.Configuration}`}>
-      <Card.Header as={'h5'} className={'fw-light'}>
-        Configuration
-      </Card.Header>
-      <Card.Body>
-        <dl>
-          {context.tournament.config_items.map((item) => {
-            return (
-              <ConfigItemForm item={item} key={item.key} />
-            )
-          })}
-        </dl>
-      </Card.Body>
-    </Card>
+    <ErrorBoundary>
+      <Card className={`${classes.Card} ${classes.Configuration}`}>
+        <Card.Header as={'h5'} className={'fw-light'}>
+          Configuration
+        </Card.Header>
+        <Card.Body>
+          <dl>
+            {context.tournament.config_items.map((item) => {
+              return (
+                <ConfigItemForm item={item} key={item.key} />
+              )
+            })}
+          </dl>
+        </Card.Body>
+      </Card>
+    </ErrorBoundary>
   );
 }
 
