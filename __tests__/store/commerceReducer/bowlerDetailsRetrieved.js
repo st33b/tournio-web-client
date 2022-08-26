4b06d15bc895a8768b81740dad1615bdbd691dfd
Scott Stebleton <scott@stebleton.net>
2022-08-26 13:15:49 -0400

    fix that whoopsie

---
+++ __tests__/store/commerceReducer/bowlerDetailsRetrieved.js
@@ -0,0 +1,130 @@
+import * as actionTypes from '../../../src/store/actions/actionTypes';
+import {commerceReducer} from "../../../src/store/commerceReducer";
+
+describe ('action type: tournament details retrieved', () => {
+  const paidPurchases = [
+    {
+      name: 'ticket',
+    },
+    {
+      name: 'silly hat',
+    }
+  ];
+  const unpaidNonLedgerItems = [
+    {
+      name: 'a flag',
+      category: 'identity',
+    },
+  ];
+  const unpaidLedgerItems = [
+    {
+      name: 'entry fee',
+      category: 'ledger',
+    },
+  ]
+  const actionBowler = {
+    // these are expected to be there
+    has_free_entry: false,
+    paid_purchases: paidPurchases,
+    unpaid_purchases: unpaidLedgerItems.concat(unpaidNonLedgerItems),
+
+    // these are things added just for the fun of testing
+    name: 'Captain Marvel',
+    average: 300,
+    usbc_id: '1-1',
+    powers: [
+      'flight',
+      'indestructible',
+      'photon beam',
+      'strong',
+      'glows',
+      "puts up with men's shit",
+    ],
+  };
+  const availableItems = [
+    {
+      id: 1,
+      name: 'shirt',
+    },
+    {
+      id: 2,
+      name: 'shoes',
+    },
+    {
+      id: 3,
+      name: 'service',
+    },
+  ];
+  const action = {
+    type: actionTypes.BOWLER_DETAILS_RETRIEVED,
+    bowler: actionBowler,
+    availableItems: availableItems,
+  }
+
+  //
+  // Presence of properties
+  //
+  it ('includes a bowler property in the return', () => {
+    const result = commerceReducer({}, action);
+    expect(result.bowler).toBeDefined();
+  });
+  it ('includes an availableItems property in the return', () => {
+    const result = commerceReducer({}, action);
+    expect(result.availableItems).toBeDefined();
+  });
+  it ('includes a cart property in the return', () => {
+    const result = commerceReducer({}, action);
+    expect(result.cart).toBeDefined();
+  });
+  it ('includes a purchasedItems property in the return', () => {
+    const result = commerceReducer({}, action);
+    expect(result.purchasedItems).toBeDefined();
+  });
+  it ('includes a freeEntry property in the return', () => {
+    const result = commerceReducer({}, action);
+    expect(result.freeEntry).toBeDefined();
+  });
+  it ('includes an error property in the return', () => {
+    const result = commerceReducer({}, action);
+    expect(result.error).toBeDefined();
+  });
+
+  //
+  // Nulled-out properties
+  //
+  it ('nulls the freeEntry property in the return', () => {
+    const result = commerceReducer({}, action);
+    expect(result.freeEntry).toBeNull();
+  });
+  it ('nulls the error property in the return', () => {
+    const result = commerceReducer({}, action);
+    expect(result.error).toBeNull();
+  });
+
+  //
+  // Objects in the response
+  //
+  it ('includes the retrieved bowler in the returned object', () => {
+    const result = commerceReducer({}, action);
+    expect(result.bowler).toStrictEqual(actionBowler);
+  });
+  it ('includes available items in the returned object', () => {
+    const result = commerceReducer({}, action);
+    expect(result.availableItems).toStrictEqual(availableItems);
+  });
+  it ('includes purchased items in the returned object', () => {
+    const result = commerceReducer({}, action);
+    expect(result.purchasedItems).toStrictEqual(paidPurchases);
+  });
+  it ('includes all cart items in the returned object', () => {
+    const result = commerceReducer({}, action);
+    expect(result.cart).toStrictEqual(unpaidLedgerItems.concat(unpaidNonLedgerItems));
+  })
+
+  // special case: the bowler has a free entry
+  it ('includes only non-ledger items in the cart', () => {
+    action.bowler.has_free_entry = true;
+    const result = commerceReducer({}, action);
+    expect(result.cart).toStrictEqual(unpaidNonLedgerItems);
+  })
+});
