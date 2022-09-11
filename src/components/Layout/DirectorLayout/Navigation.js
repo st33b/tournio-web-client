07c883c4da0917d2217a46e9cc4dd4a3c10d06c2
Scott Stebleton <scott@stebleton.net>
2022-09-11 18:29:41 -0400

    more migration toward director reducer

--- src/components/Layout/DirectorLayout/Navigation.js
+++ src/components/Layout/DirectorLayout/Navigation.js
@@ -4,17 +4,52 @@ import {Container, Nav, Navbar} from "react-bootstrap";
 import {useDirectorContext} from "../../../store/DirectorContext";
 
 import classes from './Navigation.module.scss';
+import {useClientReady} from "../../../utils";
 
 const Navigation = () => {
   const directorContext = useDirectorContext();
 
-  const [loggedIn, setLoggedIn] = useState(false);
   const [isSuperuser, setIsSuperuser] = useState(false);
 
   useEffect(() => {
-    setLoggedIn(directorContext.isLoggedIn);
     setIsSuperuser(directorContext.user && directorContext.user.role === 'superuser');
   }, [directorContext.isLoggedIn, directorContext.user]);
+
+  const ready = useClientReady();
+  if (!ready) {
+    return '';
+  }
+
+  let content = '';
+  if (directorContext.user) {
+    content = (
+      <>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id={'basic-navbar-nav'}>
+          <Nav className={'ms-4 me-auto'}>
+            {isSuperuser && (
+              <Nav.Link href={'/director/users'}>
+                User Accounts
+              </Nav.Link>
+            )}
+            {(isSuperuser || directorContext.user.tournaments.length > 1) && (
+              <Nav.Link href={'/director/tournaments'}>
+                Tournaments
+              </Nav.Link>
+            )}
+            <Nav.Link href={'/director/users/' + directorContext.user.identifier}>
+              My Profile
+            </Nav.Link>
+          </Nav>
+          <Nav className={'ms-2 ms-md-auto pe-2'}>
+            <Nav.Link href={'/director/logout'}>
+              Log Out
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </>
+    );
+  }
 
   return (
     <div className={classes.Navigation}>
@@ -31,33 +66,7 @@ const Navigation = () => {
             </span>
           </Navbar.Brand>
         </div>
-        {loggedIn && (
-          <>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id={'basic-navbar-nav'}>
-              <Nav className={'ms-4 me-auto'}>
-                {isSuperuser && (
-                  <Nav.Link href={'/director/users'}>
-                    User Accounts
-                  </Nav.Link>
-                )}
-                {(isSuperuser || directorContext.user.tournaments.length > 1) && (
-                  <Nav.Link href={'/director/tournaments'}>
-                    Tournaments
-                  </Nav.Link>
-                )}
-                <Nav.Link href={'/director/users/' + directorContext.user.identifier}>
-                  My Profile
-                </Nav.Link>
-              </Nav>
-              <Nav className={'ms-2 ms-md-auto pe-2'}>
-                <Nav.Link href={'/director/logout'}>
-                  Log Out
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </>
-        )}
+        {content}
       </Navbar>
     </div>
   );
--- src/store/DirectorContext.js
+++ src/store/DirectorContext.js
@@ -8,19 +8,15 @@ const DirectorContext = createContext({
   login: (token, userData) => {},
   logout: () => {},
   user: null,
-  tournament: null,
-  setTournament: (t) => {},
 });
 
 export const DirectorContextProvider = ({children}) => {
   let tokenData;
   let userData;
-  let tournamentData;
 
   if (typeof window !== 'undefined') {
     tokenData = localStorage.getItem('token');
     userData = JSON.parse(localStorage.getItem('currentUser'));
-    tournamentData = JSON.parse(localStorage.getItem('tournament'));
   }
 
   let initialToken, initialUser, initialTournament;
@@ -28,23 +24,17 @@ export const DirectorContextProvider = ({children}) => {
     initialToken = tokenData;
     initialUser = userData;
   }
-  if (tournamentData) {
-    initialTournament = tournamentData;
-  }
 
   const [token, setToken] = useState(initialToken);
   const [currentUser, setCurrentUser] = useState(initialUser);
-  const [tournament, setTournament] = useState(initialTournament);
 
   const userIsLoggedIn = !!token;
 
   const logoutHandler = useCallback(() => {
     setToken(null);
     setCurrentUser(null);
-    setTournament(null);
     localStorage.removeItem('token');
     localStorage.removeItem('currentUser');
-    localStorage.removeItem('tournament');
   }, []);
 
   const loginHandler = (newToken, userDetails) => {
@@ -56,7 +46,6 @@ export const DirectorContextProvider = ({children}) => {
 
   const tournamentHandler = (newTournament) => {
     console.log("Setting tournament in DirectorContext -- got another one to remove!")
-    setTournament(newTournament);
     localStorage.setItem('tournament', JSON.stringify(newTournament));
   }
 
@@ -71,12 +60,10 @@ export const DirectorContextProvider = ({children}) => {
 
   const contextValue = {
     token: token,
-    isLoggedIn: userIsLoggedIn,
+    isLoggedIn: currentUser != null,
     login: loginHandler,
     logout: logoutHandler,
     user: currentUser,
-    setTournament: tournamentHandler,
-    tournament: tournament,
 
     directorState: directorState,
     dispatch: dispatch,
