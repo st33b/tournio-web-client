f729f70464e64a5f742e163e688317276929d582
Scott Stebleton <scott@stebleton.net>
2022-09-26 18:06:41 -0400

    make the footer bar fixed even on mobile

--- src/components/Director/TournamentBuilder/FormContainer.js
+++ src/components/Director/TournamentBuilder/FormContainer.js
@@ -1,17 +1,40 @@
+import {useRouter} from "next/router";
 import {useDirectorContext} from "../../../store/DirectorContext";
 import Name from './FormSteps/Name';
 import Details from "./FormSteps/Details";
 import Dates from './FormSteps/Dates';
 import Logo from "./FormSteps/Logo";
-
-import classes from './TournamentBuilder.module.scss';
 import Scoring from "./FormSteps/Scoring";
 import RequiredEvents from "./FormSteps/RequiredEvents";
 import AdditionalEvents from "./FormSteps/AdditionalEvents";
 import DerivedEvents from "./FormSteps/DerivedEvents";
 
+import classes from './TournamentBuilder.module.scss';
+import {useEffect} from "react";
+
 const FormContainer = ({activeStep}) => {
+  const router = useRouter();
   const {directorState, dispatch} = useDirectorContext();
+
+  const SUPPORTED_STEPS = [
+    'name',
+    'details',
+    'dates',
+    'logo',
+    'scoring',
+    'required_events',
+    'additional_events',
+    'derived_events',
+  ];
+
+  useEffect(() => {
+    if (!activeStep) {
+      return;
+    }
+    if (!SUPPORTED_STEPS.includes(activeStep)) {
+      router.push('/director/tournaments');
+    }
+  }, [activeStep]);
 
   return (
     <div className={classes.FormContainer}>
--- src/components/Director/TournamentBuilder/Progress.js
+++ src/components/Director/TournamentBuilder/Progress.js
@@ -42,36 +42,56 @@ const Progress = ({activeStep}) => {
 
   const activeIndex = steps.findIndex(({key}) => key === activeStep);
 
-  let stepClass = classes.Done;
+  let activeStepText = '';
   return (
     <div className={classes.Progress}>
-      {steps.map(({key, display}, i) => {
-        const onActiveStep = i == activeIndex;
-        if (onActiveStep) {
-          stepClass = classes.Active;
-        }
-        const content = (
-          <div className={`${classes.Step} ${stepClass}`} key={key}>
-            <h6>
-              {i < activeIndex  && (
-                <a href={`/director/tournaments/new?step=${key}`}
-                   className={classes.Text}>
-                  {display}
+      <div className={'d-sm-none'}>
+        <div className={'d-flex justify-content-center'}>
+          {steps.map(({key, display}, i) => {
+            const onActiveStep = i === activeIndex;
+            const stepClass = i < activeIndex ? classes.Done : (onActiveStep ? classes.Active : classes.Upcoming);
+            const iconClass = i < activeIndex ? 'bi-check2-circle' : (onActiveStep ? 'bi-arrow-down' : 'bi-dash-circle-dotted');
+            if (onActiveStep) {
+              activeStepText = display;
+            }
+            return (
+              <div className={`flex-fill ${classes.Step} ${stepClass}`} key={key}>
+                <a href={i < activeIndex ? `/director/tournaments/new?step=${key}` : null}>
+                  <i className={iconClass} aria-hidden={true}/>
+                  <span className={'visually-hidden'}>
+                    {display}
+                  </span>
                 </a>
-              )}
-              {i >= activeIndex && (
-                <span className={classes.Text}>
-                  {display}
-                </span>
-              )}
-            </h6>
-          </div>
-        );
-        if (onActiveStep) {
-          stepClass = classes.Upcoming;
-        }
-        return content;
-      })}
+              </div>
+            );
+          })}
+        </div>
+        <h6 className={`${classes.StepText} ${classes.Active}`}>
+          {activeStepText}
+        </h6>
+      </div>
+      <div className={'d-none d-sm-block'}>
+        {steps.map(({key, display}, i) => {
+          const stepClass = i < activeIndex ? classes.Done : (i === activeIndex ? classes.Active : classes.Upcoming);
+          return (
+            <div className={`${classes.Step} ${stepClass}`} key={key}>
+              <h6>
+                {i < activeIndex && (
+                  <a href={`/director/tournaments/new?step=${key}`}
+                     className={classes.Text}>
+                    {display}
+                  </a>
+                )}
+                {i >= activeIndex && (
+                  <span className={classes.Text}>
+                    {display}
+                  </span>
+                )}
+              </h6>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
