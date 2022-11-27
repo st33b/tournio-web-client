883e6f0a0f776ba52ec26b9236450f22e3df7003
Scott Stebleton <scott@stebleton.net>
2022-11-27 14:09:08 -0500

    fix date string display on front page (#51)
    fix date string display

--- src/components/Registration/TournamentDetails/TournamentDetails.js
+++ src/components/Registration/TournamentDetails/TournamentDetails.js
@@ -317,9 +317,24 @@ const TournamentDetails = ({tournament}) => {
     );
   }
 
-  const start = new Date(tournament.start_date);
-  const end = new Date(tournament.end_date);
-  const datesString = `${format(start, 'LLLL')} ${start.getDate()}-${end.getDate()}, ${end.getFullYear()}`;
+  /*
+   * @hack To get dates to seem like they're behaving. But they're actually not.
+   * @todo Use a JS library for date/time handling that can properly distinguish dates from timestamps
+   */
+  const start = new Date(`${tournament.start_date}T12:00:00-04:00`);
+  const end = new Date(`${tournament.end_date}T12:00:00-04:00`);
+  const startMonth = format(start, 'LLLL');
+  const endMonth = format(end, 'LLLL');
+  const startYear = start.getFullYear();
+  const endYear = end.getFullYear();
+  const startDisplayYear = startYear === endYear ? '' : `, ${startYear}`;
+  const endDisplayYear = endYear;
+  let datesString = '';
+  if (startMonth === endMonth) {
+    datesString = `${startMonth} ${start.getDate()}${startDisplayYear}-${end.getDate()}, ${endDisplayYear}`;
+  } else {
+    datesString = `${startMonth} ${start.getDate()}${startDisplayYear}-${endMonth} ${end.getDate()}, ${endDisplayYear}`;
+  }
 
   return (
     <div className={classes.TournamentDetails}>
