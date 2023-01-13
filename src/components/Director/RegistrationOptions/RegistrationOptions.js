6036c47d2561f306de76536ea02270986b42c618
Scott Stebleton <scott@stebleton.net>
2023-01-12 20:51:35 -0500

    WIP -- updating shift form to omit registration options and prepare for multiple shifts

---
+++ src/components/Director/RegistrationOptions/RegistrationOptions.js
@@ -0,0 +1,126 @@
+import {useEffect, useState} from 'react';
+import {Card, ListGroup} from "react-bootstrap";
+import {Map} from 'immutable';
+
+import classes from '../TournamentInPrep/TournamentInPrep.module.scss';
+import {directorApiRequest} from "../../../director";
+import {useDirectorContext} from "../../../store/DirectorContext";
+import {tournamentDetailsRetrieved} from "../../../store/actions/directorActions";
+
+const RegistrationOptions = ({tournament}) => {
+  const context = useDirectorContext();
+
+  const REGISTRATION_TYPES = ['new_team', 'solo', 'join_team', 'partner', 'new_pair'];
+  const REGISTRATION_TYPE_LABELS = [
+    {key: 'new_team', label: 'New Teams'},
+    {key: 'solo', label: 'Solo Entries'},
+    {key: 'join_team', label: 'Join a Team'},
+    {key: 'partner', label: 'Partner Up'},
+    {key: 'new_pair', label: 'New Doubles Pair'},
+  ];
+
+  const initialFormData = Map({
+    new_team: false,
+    join_team: true,
+    solo: true,
+    partner: false,
+    new_pair: false,
+  });
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  // Populate form data with what the tournament already has
+  useEffect(() => {
+    if (!tournament) {
+      return;
+    }
+    const newFormData = formData.withMutations(map => {
+      REGISTRATION_TYPES.forEach(rType => {
+        map.set(rType, tournament.registration_options[rType]);
+      });
+    });
+    setFormData(Map(newFormData));
+  }, [tournament]);
+
+  if (!tournament) {
+    return '';
+  }
+
+  const optionToggled = (event) => {
+    const inputName = event.target.name;
+    const newValue = event.target.checked;
+    const newFormData = formData.set(inputName, newValue);
+    setFormData(newFormData);
+    submitRegistrationOptions(newFormData);
+  }
+
+  const submitRegistrationOptions = (options) => {
+    const uri = `/director/tournaments/${tournament.identifier}`;
+    const enabledTypes = [];
+    REGISTRATION_TYPES.forEach(rType => {
+      if (options.get(rType)) {
+        enabledTypes.push(rType);
+      }
+    });
+    const requestConfig = {
+      method: 'patch',
+      data: {
+        tournament: {
+          details: {
+            enabled_registration_options: enabledTypes,
+          },
+        },
+      },
+    };
+    directorApiRequest({
+      uri: uri,
+      requestConfig: requestConfig,
+      context: context,
+      onSuccess: (tournament) => { context.dispatch(tournamentDetailsRetrieved(tournament)) },
+      onFailure: (data) => setErrorMessage(data.error),
+    })
+  }
+
+  return (
+    <Card className={'mb-3'}>
+      <Card.Header as={'h5'} className={'fw-light'}>
+        Registration Options
+      </Card.Header>
+      <Card.Body>
+        {REGISTRATION_TYPE_LABELS.map(kind => (
+          <div className={'form-check form-switch'} key={kind.key}>
+            <input type={'checkbox'}
+                   className={'form-check-input'}
+                   role={'switch'}
+                   id={kind.key}
+                   name={kind.key}
+                   checked={formData.get(kind.key)}
+                   onChange={optionToggled}/>
+            <label htmlFor={kind.key}
+                   className={'form-check-label'}>
+              {kind.label}
+            </label>
+          </div>
+        ))}
+
+      </Card.Body>
+      {errorMessage && (
+        <div className={'alert alert-danger alert-dismissible fade show d-flex align-items-center mt-3 mx-3'}
+             role={'alert'}>
+          <i className={'bi-check2-circle pe-2'} aria-hidden={true}/>
+          <div className={'me-auto'}>
+            {errorMessage}
+            <button type="button"
+                    className={"btn-close"}
+                    data-bs-dismiss="alert"
+                    onClick={() => setErrorMessage(null)}
+                    aria-label="Close"/>
+          </div>
+        </div>
+      )}
+    </Card>
+  );
+}
+
+export default RegistrationOptions;
--- src/components/Director/ShiftForm/ShiftForm.js
+++ src/components/Director/ShiftForm/ShiftForm.js
@@ -15,32 +15,17 @@ import {
 const ShiftForm = ({tournament, shift}) => {
   const context = useDirectorContext();
 
-  const REGISTRATION_TYPES = ['new_team', 'solo', 'join_team', 'partner', 'new_pair'];
-  const REGISTRATION_TYPE_LABELS = [
-    {key: 'new_team', label: 'New Teams'},
-    {key: 'solo', label: 'Solo Entries'},
-    {key: 'join_team', label: 'Join a Team'},
-    {key: 'partner', label: 'Partner Up'},
-    {key: 'new_pair', label: 'New Doubles Pair'},
-  ];
-
   const initialFormData = Map({
-    // name: '',
+    name: '',
+    description: '',
     capacity: 0,
-    // display_order: 1,
-    // events: [],
-    new_team: true,
-    join_team: true,
-    solo: true,
-    partner: false,
-    new_pair: false,
+    display_order: 1,
 
     valid: false,
   });
 
   const [formData, setFormData] = useState(initialFormData);
   const [formDisplayed, setFormDisplayed] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
 
   // Populate form data with the shift prop
@@ -49,16 +34,13 @@ const ShiftForm = ({tournament, shift}) => {
       return;
     }
     const existingShift = {
-      // name: shift.name,
+      name: shift.name,
+      description: shift.description,
       capacity: shift.capacity,
-      // display_order: shift.display_order,
-      // events: shift.events,
+      display_order: shift.display_order,
 
       valid: true,
     };
-    REGISTRATION_TYPES.forEach(rType => {
-      existingShift[rType] = shift.registration_types[rType];
-    })
 
     setFormData(Map(existingShift));
   }, [shift]);
@@ -66,15 +48,6 @@ const ShiftForm = ({tournament, shift}) => {
   if (!context || !tournament) {
     return '';
   }
-
-  // const addEvent = (event) => {
-  //   event.preventDefault();
-  //   const newEventsList = formData.get('events').concat({event: '', day: '', time: ''});
-  //   const newFormData = formData.withMutations(map => {
-  //     map.set('events', newEventsList).set('valid', false);
-  //   });
-  //   setFormData(newFormData);
-  // }
 
   const allowDelete = shift && (tournament.state !== 'active' && tournament.state !== 'demo');
 
@@ -93,29 +66,14 @@ const ShiftForm = ({tournament, shift}) => {
     let inputName = event.target.name;
     switch (inputName) {
       case 'capacity':
-      // case 'display_order':
+      case 'display_order':
         newValue = parseInt(event.target.value);
         break;
-      case 'new_team':
-      case 'join_team':
-      case 'solo':
-      case 'partner':
-      case 'new_pair':
-        newValue = event.target.checked;
+      case 'name':
+      case 'description':
+        newValue = event.target.value;
         break;
-      // case 'name':
-      //   newValue = event.target.value;
-      //   break;
       default:
-        // TODO: fix this for tournaments with multiple shifts
-        // const parts = inputName.split('_');
-        // const index = parseInt(parts[1]);
-        // const element = parts[2];
-        // const value = event.target.value;
-        // console.log(`Event ${index} ${element}`, value);
-        // inputName = 'events';
-        // newValue = formData.get('events').slice();
-        // newValue[index][element] = value;
         break;
     }
     const newFormData = formData.withMutations(map => {
@@ -125,32 +83,22 @@ const ShiftForm = ({tournament, shift}) => {
   }
 
   const isValid = (data) => {
-    // TODO: accommodate tournaments with multiple shifts
-    // return data.get('name').length > 0 &&
-    return data.get('capacity') > 0; // && data.get('display_order') > 0;
+    return data.get('capacity') > 0 &&
+      data.get('display_order') > 0;
   }
 
   const addShiftFormSubmitted = (event) => {
     event.preventDefault();
 
     const uri = `/director/tournaments/${tournament.identifier}/shifts`;
-    const registrationTypes = [];
-    REGISTRATION_TYPES.forEach(rType => {
-      if (formData.get(rType)) {
-        registrationTypes.push(rType);
-      }
-    })
     const requestConfig = {
       method: 'post',
       data: {
         shift: {
           capacity: formData.get('capacity'),
-          // name: formData.get('name'),
-          // display_order: formData.get('display_order'),
-          details: {
-            // events: formData.get('events'),
-            registration_types: registrationTypes,
-          }
+          name: formData.get('name'),
+          description: formData.get('description'),
+          display_order: formData.get('display_order'),
         }
       }
     };
@@ -190,23 +138,14 @@ const ShiftForm = ({tournament, shift}) => {
     event.preventDefault();
 
     const uri = `/director/shifts/${shift.identifier}`;
-    const registrationTypes = [];
-    REGISTRATION_TYPES.forEach(rType => {
-      if (formData.get(rType)) {
-        registrationTypes.push(rType);
-      }
-    })
     const requestConfig = {
       method: 'patch',
       data: {
         shift: {
           capacity: formData.get('capacity'),
-          // name: formData.get('name'),
-          // display_order: formData.get('display_order'),
-          details: {
-            registration_types: registrationTypes,
-            // events: formData.get('events'),
-          }
+          name: formData.get('name'),
+          description: formData.get('description'),
+          display_order: formData.get('display_order'),
         }
       }
     };
@@ -241,16 +180,6 @@ const ShiftForm = ({tournament, shift}) => {
     outerClasses.push(classes.FormDisplayed);
   }
 
-  // const daysOfWeek = [
-  //   {value: 'Monday', display: 'Mon'},
-  //   {value: 'Tuesday', display: 'Tue'},
-  //   {value: 'Wednesday', display: 'Wed'},
-  //   {value: 'Thursday', display: 'Thu'},
-  //   {value: 'Friday', display: 'Fri'},
-  //   {value: 'Saturday', display: 'Sat'},
-  //   {value: 'Sunday', display: 'Sun'},
-  // ];
-
   return (
     <div className={outerClasses.join(' ')}>
       {!formDisplayed && !shift &&
@@ -272,35 +201,30 @@ const ShiftForm = ({tournament, shift}) => {
            title={'Edit details'}
            onClick={toggleEdit}>
           <dl className={`${classes.ExistingShift} px-2`}>
-            {/*<div className={'row'}>*/}
-            {/*  <dt className={'col-4'}>*/}
-            {/*    Order*/}
-            {/*  </dt>*/}
-            {/*  <dd className={'col'}>*/}
-            {/*    {shift.display_order}*/}
-            {/*  </dd>*/}
-            {/*</div>*/}
-            {/*<div className={'row'}>*/}
-            {/*  <dt className={'col-4'}>*/}
-            {/*    Name*/}
-            {/*  </dt>*/}
-            {/*  <dd className={'col'}>*/}
-            {/*    {shift.name}*/}
-            {/*  </dd>*/}
-            {/*</div>*/}
-
-            {/*<div className={'row'}>*/}
-            {/*  <dt className={'col-4'}>*/}
-            {/*    Events*/}
-            {/*  </dt>*/}
-            {/*  <dd className={'col'}>*/}
-            {/*    {shift.events.map(event => (*/}
-            {/*      <p className={'mb-2'}>*/}
-            {/*        {event.event}: {event.day}, {event.time}*/}
-            {/*      </p>*/}
-            {/*    ))}*/}
-            {/*  </dd>*/}
-            {/*</div>*/}
+            <div className={'row'}>
+              <dt className={'col-4'}>
+                Order
+              </dt>
+              <dd className={'col'}>
+                {shift.display_order}
+              </dd>
+            </div>
+            <div className={'row'}>
+              <dt className={'col-4'}>
+                Name
+              </dt>
+              <dd className={'col'}>
+                {shift.name}
+              </dd>
+            </div>
+            <div className={'row'}>
+              <dt className={'col-4'}>
+                Description
+              </dt>
+              <dd className={'col'}>
+                {shift.description}
+              </dd>
+            </div>
 
             <div className={`row ${colorClass} g-3`}>
               <dt className={'col-6 mt-2'}>
@@ -326,130 +250,55 @@ const ShiftForm = ({tournament, shift}) => {
                 {shift.requested_count}
               </dd>
             </div>
-
-            <div className={'row g-3'}>
-              <dt className={'col-6'}>
-                Enabled Entry Types
-              </dt>
-              <dd className={'col'}>
-                {REGISTRATION_TYPE_LABELS.map(kind => (
-                  <div className={'d-block'} key={kind.key}>
-                    {shift.registration_types[kind.key] && (
-                      <span className={'pe-1'}>
-                        <i className={'bi-check-lg text-success'} aria-hidden={true}/>
-                        <span className={'visually-hidden'}>Yes</span>
-                      </span>
-                    )}
-                    {!shift.registration_types[kind.key] && (
-                      <span className={'pe-1'}>
-                        <i className={'bi-x-lg text-danger'} aria-hidden={true}/>
-                        <span className={'visually-hidden'}>No</span>
-                      </span>
-                    )}
-                    {kind.label}
-                  </div>
-                ))}
-              </dd>
-            </div>
           </dl>
         </a>
       }
       {formDisplayed &&
         <Card.Body>
           <form onSubmit={submitFunction}>
-            {/*<div className={'row mb-3'}>*/}
-            {/*  <label htmlFor={'name'}*/}
-            {/*         className={'form-label mb-1'}>*/}
-            {/*    Name*/}
-            {/*  </label>*/}
-            {/*  <div className={'col'}>*/}
-            {/*    <input type={'text'}*/}
-            {/*           className={'form-control'}*/}
-            {/*           name={'name'}*/}
-            {/*           id={'name'}*/}
-            {/*           required={false}*/}
-            {/*           placeholder={'only necessary with multiple shifts'}*/}
-            {/*           onChange={inputChanged}*/}
-            {/*           value={formData.get('name')}*/}
-            {/*    />*/}
-            {/*  </div>*/}
-            {/*</div>*/}
-
-            {/*<div className={'row mb-3'}>*/}
-            {/*  <label className={'form-label mb-1 col-12'}>*/}
-            {/*    Events*/}
-            {/*  </label>*/}
-            {/*  {formData.get('events').length === 0 && (*/}
-            {/*    <div className={`${classes.SubLabel} row m-0 py-0 ps-3 pe-0 ms-1`}>*/}
-            {/*      <label>*/}
-            {/*        (only necessary with multiple shifts)*/}
-            {/*      </label>*/}
-            {/*    </div>*/}
-            {/*  )}*/}
-            {/*  {formData.get('events').length > 0 && (*/}
-            {/*    <div className={`${classes.SubLabel} row m-0 py-0 ps-3 pe-0 ms-1`}>*/}
-            {/*      <div className={'col-5 p-0'}>*/}
-            {/*        <label>Name</label>*/}
-            {/*      </div>*/}
-            {/*      <div className={'col-3 p-0'}>*/}
-            {/*        <label>Day</label>*/}
-            {/*      </div>*/}
-            {/*      <div className={'col py-0 pe-3 ps-0'}>*/}
-            {/*        <label>Time</label>*/}
-            {/*      </div>*/}
-            {/*    </div>*/}
-            {/*  )}*/}
-            {/*  {formData.get('events').map((event, i) => (*/}
-            {/*    <div key={i} className={'row m-0 py-0 ps-3 pe-0'}>*/}
-            {/*      <div className={'col-5 p-0'}>*/}
-            {/*        <input type={'text'}*/}
-            {/*               className={'form-control'}*/}
-            {/*               id={`events_${i}_event`}*/}
-            {/*               placeholder={'Name'}*/}
-            {/*               name={`events_${i}_event`}*/}
-            {/*               value={formData.get('events')[i]['event']}*/}
-            {/*               onChange={inputChanged}/>*/}
-            {/*      </div>*/}
-            {/*      <div className={'col-3 p-0'}>*/}
-            {/*        <select className={'form-select'}*/}
-            {/*                id={`events_${i}_day`}*/}
-            {/*                name={`events_${i}_day`}*/}
-            {/*                onChange={inputChanged}>*/}
-            {/*          {daysOfWeek.map((day, j) => (*/}
-            {/*            <option key={j}*/}
-            {/*                    value={day.value}*/}
-            {/*                    selected={formData.get('events')[i]['day'] === day.value}>*/}
-            {/*              {day.display}*/}
-            {/*            </option>*/}
-            {/*          ))}*/}
-            {/*        </select>*/}
-            {/*      </div>*/}
-            {/*      <div className={'col py-0 pe-3 ps-0'}>*/}
-            {/*        <input type={'text'}*/}
-            {/*               className={'form-control'}*/}
-            {/*               id={`events_${i}_time`}*/}
-            {/*               placeholder={'Time'}*/}
-            {/*               name={`events_${i}_time`}*/}
-            {/*               value={formData.get('events')[i]['time']}*/}
-            {/*               onChange={inputChanged}/>*/}
-            {/*      </div>*/}
-            {/*    </div>*/}
-            {/*  ))}*/}
-            {/*  <a href={'#'}*/}
-            {/*     className={`${classes.AddEvent} ps-3 ms-1 pt-1`}*/}
-            {/*     onClick={addEvent}>*/}
-            {/*    add event*/}
-            {/*  </a>*/}
-            {/*</div>*/}
+            <div className={'row mb-3'}>
+              <label htmlFor={'name'}
+                     className={'form-label mb-1 col-12'}>
+                Name
+              </label>
+              <div className={'col-12'}>
+                <input type={'text'}
+                       className={'form-control'}
+                       name={'name'}
+                       id={'name'}
+                       required={false}
+                       placeholder={'only necessary with multiple shifts'}
+                       onChange={inputChanged}
+                       value={formData.get('name')}
+                />
+              </div>
+            </div>
 
             <div className={'row mb-3'}>
-              <div className={'col-5'}>
-                <label htmlFor={'capacity'} >
-                  Capacity (bowlers)
-                </label>
+              <label htmlFor={'description'}
+                     className={'form-label mb-1 col-12'}>
+                Description
+              </label>
+              <div className={'col-12'}>
+                <input type={'text'}
+                       className={'form-control'}
+                       name={'description'}
+                       id={'description'}
+                       required={false}
+                       placeholder={'only necessary with multiple shifts'}
+                       onChange={inputChanged}
+                       value={formData.get('description')}
+                />
               </div>
-              <div className={'col'}>
+            </div>
+
+            <div className={'row mb-3'}>
+              <label htmlFor={'capacity'} className={'form-label col-7'}>
+                Capacity (bowlers)
+              </label>
+              <div className={'col-5'}>
                 <input type={'number'}
+                       min={1}
                        className={'form-control'}
                        name={'capacity'}
                        id={'capacity'}
@@ -458,44 +307,25 @@ const ShiftForm = ({tournament, shift}) => {
                        value={formData.get('capacity')}
                 />
               </div>
-              {/*<div className={'col-6'}>*/}
-              {/*  <label htmlFor={'display_order'} className={'form-label ps-0 mb-1'}>*/}
-              {/*    Display Order*/}
-              {/*  </label>*/}
-              {/*  <input type={'number'}*/}
-              {/*         className={'form-control'}*/}
-              {/*         name={'display_order'}*/}
-              {/*         id={'display_order'}*/}
-              {/*         required={true}*/}
-              {/*         onChange={inputChanged}*/}
-              {/*         value={formData.get('display_order')}*/}
-              {/*  />*/}
-              {/*</div>*/}
             </div>
+
             <div className={'row mb-3'}>
+              <label htmlFor={'display_order'} className={'form-label col-7'}>
+                Display Order
+              </label>
               <div className={'col-5'}>
-                <label>
-                  Enabled Registration Routes
-                </label>
-              </div>
-              <div className={'col'}>
-                {REGISTRATION_TYPE_LABELS.map(kind => (
-                  <div className={'form-check form-switch'} key={kind.key}>
-                    <input type={'checkbox'}
-                           className={'form-check-input'}
-                           role={'switch'}
-                           id={kind.key}
-                           name={kind.key}
-                           checked={formData.get(kind.key)}
-                           onChange={inputChanged}/>
-                    <label htmlFor={kind.key}
-                           className={'form-check-label'}>
-                      {kind.label}
-                    </label>
-                  </div>
-                ))}
+                <input type={'number'}
+                       min={1}
+                       className={'form-control'}
+                       name={'display_order'}
+                       id={'display_order'}
+                       required={true}
+                       onChange={inputChanged}
+                       value={formData.get('display_order')}
+                />
               </div>
             </div>
+
             <div className={'row'}>
               <div className={'d-flex justify-content-end'}>
                 {allowDelete && (
@@ -544,8 +374,6 @@ const ShiftForm = ({tournament, shift}) => {
           </div>
         </div>
       )}
-
-      {errorMessage}
     </div>
   )
 }
