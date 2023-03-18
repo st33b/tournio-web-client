020fb061fdfe5a21735f061afc6bca82aadc4207
Scott Stebleton <scott@stebleton.net>
2023-03-18 16:30:53 -0400

    remove pre-fetches from bowler listing

--- src/components/ui/SortableTableHeader/SortableTableHeader.js
+++ src/components/ui/SortableTableHeader/SortableTableHeader.js
@@ -3,14 +3,12 @@ import classes from "./SortableTableHeader.module.scss";
 // column is a Column instance from react-table
 const SortableTableHeader = ({column, text}) => {
   const headerClassName = column.canSort ? 'link-primary' : '';
+  const caretClass = column.isSortedDesc ? 'bi-caret-down-fill' : 'bi-caret-up-fill';
+  const iconClass = column.isSorted ? classes.SortIndicator + ' ' + caretClass : 'visually-hidden';
   return (
-    <span className={`${classes.SortableTableHeader} ${headerClassName}`}>
-        {text}
-      <i className={column.isSorted
-        ? classes.SortIndicator + ' ' + (column.isSortedDesc ? 'bi-caret-down-fill' : 'bi-caret-up-fill')
-        : 'visually-hidden'
-      }
-      />
+    <span className={`text-nowrap ${classes.SortableTableHeader} ${headerClassName}`}>
+      {text}
+      <i className={iconClass} />
     </span>
   );
 };
