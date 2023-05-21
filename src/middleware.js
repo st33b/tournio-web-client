8388d5097425855ec623cba009e7ddf4512fbe6d
Scott Stebleton <scott@stebleton.net>
2023-05-21 13:47:04 -0400

    Use edge config for maintenance mode, but only in production environment

---
+++ src/middleware.js
@@ -0,0 +1,21 @@
+import {NextResponse} from "next/server";
+import {get} from "@vercel/edge-config";
+
+export const config = {
+  matcher: ['/(tourn|bowl|dir|team)(.+)'], // Any page that we expect to be requested, other than / and /about
+};
+
+export async function middleware(request) {
+  if (process.env.NODE_ENV !== 'production') {
+    return;
+  }
+  
+  const inMaintenanceMode = await get('maintenanceMode');
+
+  // If we are, then re-route to maintenance
+  if (inMaintenanceMode) {
+    request.nextUrl.pathname = '/maint.html';
+
+    return NextResponse.rewrite(request.nextUrl);
+  }
+}
