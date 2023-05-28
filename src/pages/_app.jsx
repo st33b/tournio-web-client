4a3fac84b1667ef3c61081ca3245875edacf12e6
Scott Stebleton <scott@stebleton.net>
2023-05-28 08:44:21 -0400

    Remove maintenance announcement cookie when version has changed

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import {useEffect} from "react";
 import { Analytics } from "@vercel/analytics/react";
+import Cookies from "js-cookie";
 
 import '../scss/styles.scss';
 
@@ -8,6 +9,7 @@ import {RegistrationContextProvider} from "../store/RegistrationContext";
 import {CommerceContextProvider} from "../store/CommerceContext";
 
 const APP_VERSION = 'app_version';
+const MAINTENANCE_COOKIE_NAME = 'tournio-maintenance-dismissed';
 
 function MyApp({Component, pageProps})  {
   useEffect(() => {
@@ -16,6 +18,8 @@ function MyApp({Component, pageProps})  {
     const myVersion = localStorage.getItem(APP_VERSION);
     if (myVersion === null || myVersion !== process.env.NEXT_PUBLIC_TOURNIO_CLIENT_VERSION) {
       localStorage.setItem(APP_VERSION, process.env.NEXT_PUBLIC_TOURNIO_CLIENT_VERSION);
+      Cookies.remove(MAINTENANCE_COOKIE_NAME);
+      // And any other resources we should reset upon deployment of a new version
     }
   }, []);
 
