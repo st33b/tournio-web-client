ffe2e8b54186878bf2a018fcf3239e6e2b2b30af
Scott Stebleton <scott@stebleton.net>
2023-06-11 13:30:11 -0400

    WIP -- putting the global filter in place

--- src/components/Registration/BowlerList/BowlerList.js
+++ src/components/Registration/BowlerList/BowlerList.js
@@ -1,7 +1,13 @@
 import classes from "./BowlerList.module.scss";
 import {useEffect, useMemo, useRef, useState} from 'react';
 import {Link} from 'next';
-import {createColumnHelper, flexRender, getCoreRowModel, useReactTable} from "@tanstack/react-table";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  useReactTable
+} from "@tanstack/react-table";
 
 const BowlerList = ({bowlers = [], caption, includeMenuLink}) => {
   // identifier
@@ -19,22 +25,27 @@ const BowlerList = ({bowlers = [], caption, includeMenuLink}) => {
       // cell: props => <a href={`/bowlers/${props.row.original.identifier}`}>{props.getValue()}</a>,
       // cell: props => <Link href={`/bowlers/${props.row.original.identifier}`}>${props.getValue()}</Link>,
       header: 'Name',
+      enableGlobalFilter: true,
     }),
     columnHelper.accessor('usbcId', {
       cell: info => info.getValue(),
       header: 'USBC ID',
+      enableGlobalFilter: true,
     }),
     // Do we want a link to the team page?
     columnHelper.accessor('teamName', {
       cell: info => info.getValue(),
       header: 'Team Name',
+      enableGlobalFilter: true,
     }),
     columnHelper.accessor('doublesPartner', {
       cell: info => info.getValue(),
       header: 'Doubles Partner',
+      enableGlobalFilter: false,
     }),
     columnHelper.accessor('registeredOn', {
       header: 'Date Registered',
+      enableGlobalFilter: false,
     }),
     // This will give us a link/button to the bowler's menu page
     // columnHelper.display({
@@ -43,14 +54,44 @@ const BowlerList = ({bowlers = [], caption, includeMenuLink}) => {
     // }),
   ];
 
+  const [globalFilter, setGlobalFilter] = useState('');
+
   const theTable = useReactTable({
     data: bowlers,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    globalFilterFn: 'includesString',
+    state: {
+      globalFilter,
+    },
+    onGlobalFilterChange: setGlobalFilter,
+    getFilteredRowModel: getFilteredRowModel(),
   });
 
   return (
     <div className={classes.BowlerList}>
+      <form noValidate>
+        <fieldset>
+          <legend>
+            Search!
+          </legend>
+          <div className={`row mb-3`}>
+            <label className={`col-form-label col-3`} htmlFor={`searchField`}>
+              Look for:
+            </label>
+            <div className={`col-9`}>
+              <input type={'text'}
+                     className={`form-control`}
+                     id={`searchField`}
+                     placeholder={`name, USBC id, ...`}
+                     onChange={(e) => setGlobalFilter(e.target.value)}
+                     value={globalFilter}
+              />
+            </div>
+          </div>
+        </fieldset>
+      </form>
+
       <div className={`d-md-none`}>
         <ul className={`list-group list-group-flush`}>
           {theTable.getRowModel().rows.map(row => (
