a005f5807e9cbb1f0738691a1833ed266b973dd2
Scott Stebleton <scott@stebleton.net>
2023-06-20 17:54:14 -0400

    add a me page under users

---
+++ src/pages/director/users/me.js
@@ -0,0 +1,31 @@
+import {useLoginContext} from "../../../store/LoginContext";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
+import DirectorLayout from "../../../components/Layout/DirectorLayout/DirectorLayout";
+import Page from "./[identifier]";
+import {devConsoleLog} from "../../../utils";
+
+const Me = () => {
+  const {user, ready} = useLoginContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!ready || !router || !user) {
+      return;
+    }
+
+    router.replace(`/director/users/${user.identifier}`);
+  }, [ready, router, user]);
+
+  return '';
+}
+
+Me.getLayout = function getLayout(page) {
+  return (
+    <DirectorLayout>
+      {page}
+    </DirectorLayout>
+  );
+}
+
+export default Me;
