d84c3db91b30b2acd1b43aa81c668aeed44a47f6
Scott Stebleton <scott@stebleton.net>
2023-06-23 10:41:56 -0400

    hook up teams index for updating data

--- src/pages/director/tournaments/[identifier]/teams/index.js
+++ src/pages/director/tournaments/[identifier]/teams/index.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import {useRouter} from "next/router";
-import {Col, Row} from "react-bootstrap";
+import {Card, Col, Row} from "react-bootstrap";
 
-import {useDirectorApi, useTournament} from "../../../../../director";
+import {directorApiRequest, useDirectorApi, useTournament} from "../../../../../director";
 import DirectorLayout from "../../../../../components/Layout/DirectorLayout/DirectorLayout";
 import TeamListing from "../../../../../components/Director/TeamListing/TeamListing";
 import Breadcrumbs from "../../../../../components/Director/Breadcrumbs/Breadcrumbs";
@@ -10,15 +10,55 @@ import LoadingMessage from "../../../../../components/ui/LoadingMessage/LoadingM
 import ErrorBoundary from "../../../../../components/common/ErrorBoundary";
 import SuccessAlert from "../../../../../components/common/SuccessAlert";
 import ErrorAlert from "../../../../../components/common/ErrorAlert";
+import {useLoginContext} from "../../../../../store/LoginContext";
+import NewTeamForm from "../../../../../components/Director/NewTeamForm/NewTeamForm";
 
 const TeamsIndex = () => {
   const router = useRouter();
+  const {authToken} = useLoginContext();
   const {identifier, deleteSuccess} = router.query;
+  const [successMessage, setSuccessMessage] = useState();
 
-  const {loading: tournamentLoading, tournament} = useTournament();
-  const {loading: teamsLoading, data: teams, error} = useDirectorApi({
+  const {loading: tournamentLoading, tournament, tournamentUpdatedQuietly} = useTournament();
+  const {loading: teamsLoading, data: teams, error, onDataUpdate: onTeamsUpdate} = useDirectorApi({
     uri: identifier ? `/tournaments/${identifier}/teams` : null,
   });
+
+  const newTeamSubmitSuccess = (data) => {
+    const newTeams = teams.concat(data);
+    onTeamsUpdate(newTeams);
+    const newTournament = updateObject(tournament, {
+      team_count: tournament.team_count + 1,
+    });
+    tournamentUpdatedQuietly(newTournament);
+    setSuccessMessage('Team created.');
+  }
+
+  const newTeamSubmitted = (teamName) => {
+    const uri = `/tournaments/${identifier}/teams`;
+    const requestConfig = {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        team: {
+          name: teamName,
+          options: {
+            place_with_others: true
+          },
+        }
+      }
+    }
+    directorApiRequest({
+      uri: uri,
+      requestConfig: requestConfig,
+      authToken: authToken,
+      onSuccess: newTeamSubmitSuccess,
+    });
+  }
+
+  ////////////////////
 
   if (tournamentLoading || teamsLoading) {
     return <LoadingMessage message={'Retrieving team data...'} />
@@ -28,8 +68,6 @@ const TeamsIndex = () => {
     return <LoadingMessage message={'Making sense of team data...'} />
   }
 
-  ////////////////////
-
   const ladder = [{text: 'Tournaments', path: '/director'}];
   ladder.push({text: tournament.name, path: `/director/tournaments/${identifier}`});
 
@@ -37,15 +75,29 @@ const TeamsIndex = () => {
     <ErrorBoundary>
       <Breadcrumbs ladder={ladder} activeText={'Teams'}/>
       <Row>
-        <Col>
+        <Col xs={{span: 12, order: 2}} md={{span: 8, order: 1}}>
           {deleteSuccess && (
             <SuccessAlert message={'The team has been removed.'}
                           onClose={() => router.replace(router.pathname, null, {shallow: true})}/>
           )}
+          <SuccessAlert message={successMessage}
+                        className={``}
+                        onClose={() => {setSuccessMessage(null)}}
+                        />
           {error && (
             <ErrorAlert message={error.message} className={'mx-3 mt-3'}/>
           )}
           <TeamListing teams={teams} />
+        </Col>
+        <Col xs={{span: 12, order: 1}} md={{span: 4, order: 2}}>
+          <Card className={`mb-3`}>
+            <Card.Header as={'h5'} className={'fw-light'}>
+              New Team
+            </Card.Header>
+            <Card.Body>
+              <NewTeamForm submitted={newTeamSubmitted} />
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </ErrorBoundary>
