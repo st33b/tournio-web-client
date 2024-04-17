db51cb8860c16894c5defa8a0f36b7426ca47b3a
Scott Stebleton <scott@stebleton.net>
2024-04-17 17:03:14 -0400

    make sure bulk sign-in sheets appear

--- src/pages/director/tournaments/[identifier]/sign-in-sheets.js
+++ src/pages/director/tournaments/[identifier]/sign-in-sheets.js
@@ -2,7 +2,7 @@ import React from "react";
 import {useRouter} from "next/router";
 
 import SignInSheet from "../../../../components/Director/SignInSheet/SignInSheet";
-import {useDirectorApi} from "../../../../director";
+import {useDirectorApi, useModernTournament} from "../../../../director";
 import LoadingMessage from "../../../../components/ui/LoadingMessage/LoadingMessage";
 import ErrorBoundary from "../../../../components/common/ErrorBoundary";
 import {LoginContextProvider} from "../../../../store/LoginContext";
@@ -14,16 +14,24 @@ const Page = () => {
   const {loading, data: bowlers} = useDirectorApi( {
     uri: identifier ? `/tournaments/${identifier}/bowlers?include_details=true` : null,
   });
+  const {loading: tournamentLoading, tournament} = useModernTournament();
 
+  if (tournamentLoading) {
+    return <LoadingMessage message={'Retrieving the tournament, one moment please...'} />
+  }
   if (loading) {
-    return <LoadingMessage message={'Retrieving bowler data...'} />
+    return <LoadingMessage message={'Retrieving all the bowlers, one moment please...'} />
   }
 
   return (
     <div className={'container-md'}>
       <ErrorBoundary>
         {
-          bowlers && bowlers.map((bowler, i) => <SignInSheet bowler={bowler} key={bowler.identifier} showPrintButton={i === 0}/>)
+          bowlers && bowlers.map((bowler, i) => <SignInSheet
+            tournament={tournament}
+            bowler={bowler}
+            key={bowler.identifier}
+            showPrintButton={i === 0}/>)
         }
       </ErrorBoundary>
     </div>
